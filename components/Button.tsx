ff8250533a79785ad9b5302f22802d8cf694d376
Adolsik <xxxadolsxxx@poczta.fm>
2024-01-24 15:12:28 +0100

    car images api, car cards, car details

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,20 +1,34 @@
 "use client";
-import Image from "next/image"
+import Image from "next/image";
 import { MouseEventHandler } from "react";
 
-const Button = (props: {title:string, handleClick?: MouseEventHandler<HTMLButtonElement>, containerStyles?: string, btnType?: 'button' | 'submit' }) => {
+const Button = (props: {
+  title: string;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  containerStyles?: string;
+  btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
+}) => {
   return (
     <button
-     disabled={false}
-     type={"button"} 
-     className={`custom-btn ${props.containerStyles}`}
-     onClick={props.handleClick} >
-        <span className={`flex-1`}>
-            {props.title}
-        </span>
-
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${props.containerStyles}`}
+      onClick={props.handleClick}
+    >
+      <span className={`flex-1 ${props.textStyles}`}>{props.title}</span>
+      {props.rightIcon && (
+        <div className="relative w-6 h-6 ">
+          <Image src={props.rightIcon}
+           alt="right icon"
+           fill
+           className="object-contain" />
+        </div>
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- utils/index.tsx
+++ utils/index.tsx
@@ -1,12 +1,43 @@
 export async function fetchCars() {
-    const headers = {
-		'X-RapidAPI-Key': 'c4a513b7damsh17610535a826582p1b4d28jsn8f907966d112',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
-    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars`, {
-        headers: headers,
-    } );
-    const result = await response.json();
+  const headers = {
+    "X-RapidAPI-Key": "c4a513b7damsh17610535a826582p1b4d28jsn8f907966d112",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=x5`,
+    {
+      headers: headers,
+    }
+  );
+  const result = await response.json();
 
-    return result;
+  return result;
 }
+
+export const generateCarImageUrl = (
+  car: {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+  },
+  angle?: string
+) => {
+  const url = new URL("https://cdn.imagin.studio/getImage");
+  const { make, year, model } = car;
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year.toString()}`);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
+};
